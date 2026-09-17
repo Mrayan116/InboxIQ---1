3e69790439c4058bae9032bcecd95c6fca03b4df
Muhammad Rayan <rayan26278@gmail.com>
2026-09-17 17:40:25 -0400

    Update env.py

--- env.py
+++ env.py
@@ -13,7 +13,7 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
-import app.models  # noqa: F401  -- populates Base.metadata
+import app.models  # noqa: F401  -- populates Base.metadata.
 from app.core.config import settings
 from app.core.database import Base
 
